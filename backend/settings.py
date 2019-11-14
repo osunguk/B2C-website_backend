364b050b905ace490b8ced4864efa9d5a2766cc0
osunguk <osunguk@gmail.com>
2019-11-14 17:46:57 +0900

    key update

--- backend/settings.py
+++ backend/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = os.environ['SECRET_KEY']
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'io*#^)x%l-3i6gcntjb3pf_z!amzqq@zeo333vxxtitxlpkdt=')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
